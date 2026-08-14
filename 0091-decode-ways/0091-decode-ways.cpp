6330a5a3514de5413d420b4bf030c7881e4e5698
Ratnesk kumar <149929407+ratneshkumar028@users.noreply.github.com>
2026-08-14 18:35:09 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.57%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.size() + 1);
-        dp[s.size()] = 1;
-        for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] == '0') {
-                dp[i] = 0;
-            } else {
-                dp[i] = dp[i + 1];
-                if (i + 1 < s.size() && (s[i] == '1' ||
-                    s[i] == '2' && s[i + 1] < '7')) {
-                    dp[i] += dp[i + 2];
+        int n = s.size();
+        vector<int> dp(n+1);
+        dp[n] = 1;
+        for( int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+            }
+            else{
+                dp[i]=dp[i+1];
+                if(i+1<n && (s[i]=='1' || s[i]=='2' && s[i+1]<'7')){
+                    dp[i]+=dp[i+2];
+
                 }
             }
         }
